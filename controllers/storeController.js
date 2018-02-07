a6701cda074a26913eca23fc3e80041d9621d388
Candice Mayes <ca.mayes239@gmail.com>
2018-02-06 22:48:41 -0500

    finished editing flow for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -23,3 +23,25 @@ exports.getStores = async(req, res) => {
     console.log(stores);
     res.render('stores', { title: 'Stores', stores: stores })
 }
+
+exports.editStore = async (req, res) => {
+    //1. Find the store given the ID
+    const store = await Store.findOne({ _id: req.params.id });
+    //2. Confirm they are the owner of the store
+    //TODO
+    //3. Render out the edit form so that the user can update their store
+    res.render('editStore', { title: `Edit ${store.name}`, store });
+    
+}
+
+exports.updateStore = async (req, res) => {
+    //find and update the store
+    //findOndAndUpdate is a mongoDB method that takes three parameters, (query, data, options)
+    const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true, //return the new store instead of the old one
+        runValidators: true,
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+    res.redirect(`/stores/${store._id}/edit`);
+    //redirect them to the store and tell them it worked
+}
